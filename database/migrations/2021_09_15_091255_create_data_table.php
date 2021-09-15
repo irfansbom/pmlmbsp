129876a077dadd40dc493ea95fc30832e86b109d
PC-Kantor <irfansyahahmad26@gmail.com>
2021-09-15 18:21:18 +0700

    add

---
+++ database/migrations/2021_09_15_091255_create_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->text('user');
+            $table->double('dokumen_diterima');
+            $table->double('dokumen_diserahkan');
+            $table->text('deskripsi');
+            $table->string('kode_wilayah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
